722b35c990827940e9d0fb9748ed4a3a046fd94b
sanaz salemi <sanazsalemi9898>
2021-11-16 10:06:45 -0800

    hi

---
+++ src/main/java/S1.java
@@ -0,0 +1,26 @@
+public class S1 {
+
+
+    public class Main {
+
+        public static void main(String[] args) {
+
+            int[] myArray = {1, 4, 7, 9};
+            for (int i = 0; i < myArray.length; i++) {
+                if (myArray[i] == 1) {
+                    System.out.println("this is Number:One");
+                } else if (myArray[i] == 4) {
+                    System.out.println("this is Number:Four");
+                } else if (myArray[i] == 7) {
+                    System.out.println("this is Number:Seven");
+                } else if(myArray[i]==9){
+                    System.out.println("this is Number:Nine");
+                }else {
+                    System.out.println("null");
+                }
+            }
+        }
+
+    }
+
+
